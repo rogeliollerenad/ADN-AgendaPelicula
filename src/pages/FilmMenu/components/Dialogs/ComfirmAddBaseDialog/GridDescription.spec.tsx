0c6bf83bd3ddf2f1cc744d7e535e00a7193cf5c6
Rogelio Llerena Duque <rogelio.llerena@ceiba.com.co>
2021-08-31 12:05:58 -0500

    Se agrgo Test para el componente 'GridDescription'

---
+++ src/pages/FilmMenu/components/Dialogs/ComfirmAddBaseDialog/GridDescription.spec.tsx
@@ -0,0 +1,43 @@
+/* eslint-disable react/jsx-props-no-spreading, @typescript-eslint/no-var-requires, global-require */
+
+import React from 'react';
+import MockDate from 'mockdate';
+import {
+  GridDescription,
+  PropsDataDescription,
+} from 'pages/FilmMenu/components/Dialogs/ComfirmAddBaseDialog/GridDescription';
+import { render } from '@testing-library/react';
+import moment from 'moment';
+
+jest.mock('react-intersection-observer', () => {
+  const { factory } = require('tests/mocks/react-intersection-observer.mock');
+  return factory();
+});
+
+describe('GridDescription Test', () => {
+  let props: PropsDataDescription;
+
+  beforeAll(() => {
+    MockDate.set('2020-10-31');
+  });
+
+  afterAll(() => {
+    MockDate.reset();
+  });
+
+  beforeEach(() => {
+    props = {
+      description: 'Lorem Ipsum',
+      title: 'Titulo Testing',
+      released: '10/20/2000',
+      time: 120,
+      score: '3.5',
+      price: 10,
+    };
+  });
+
+  it('should match snapshot', () => {
+    const { container } = render(<GridDescription {...props} />);
+    expect(container).toMatchSnapshot();
+  });
+});
